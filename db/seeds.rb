800c471cddd055b345cca472e34a4e6d04b4aea1
Kirill qsimpleq Babikhin <kirill.babikhin@gmail.com>
2023-06-17 16:24:55 +0500

    add category seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+1.upto(10) do |_i|
+  Category.create name: Faker::Hacker.adjective
+end
