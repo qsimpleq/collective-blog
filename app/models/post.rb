f2be6e16000e6d7a17497b8aed43c14085f1cd88
Kirill qsimpleq Babikhin <kirill.babikhin@gmail.com>
2023-06-17 16:33:41 +0500

    add post validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -23,4 +23,7 @@
 class Post < ApplicationRecord
   belongs_to :category
   belongs_to :creator, class_name: 'User'
+
+  validates :title, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :body, presence: true, length: { minimum: 3 }
 end
