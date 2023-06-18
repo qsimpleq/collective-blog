aecb140c529946f807c066196c1359e9e5923813
Kirill qsimpleq Babikhin <kirill.babikhin@gmail.com>
2023-06-18 12:24:41 +0500

    add layouts i18n

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -23,7 +23,7 @@ class CategoriesController < ApplicationController
 
     respond_to do |format|
       if @category.save
-        format.html { redirect_to category_url(@category), notice: 'Category was successfully created.' }
+        format.html { redirect_to category_url(@category), notice: t('.success') }
         format.json { render :show, status: :created, location: @category }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -36,7 +36,7 @@ class CategoriesController < ApplicationController
   def update
     respond_to do |format|
       if @category.update(category_params)
-        format.html { redirect_to category_url(@category), notice: 'Category was successfully updated.' }
+        format.html { redirect_to category_url(@category), notice: t('.success') }
         format.json { render :show, status: :ok, location: @category }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -50,7 +50,7 @@ class CategoriesController < ApplicationController
     @category.destroy
 
     respond_to do |format|
-      format.html { redirect_to categories_url, notice: 'Category was successfully destroyed.' }
+      format.html { redirect_to categories_url, notice: t('.success') }
       format.json { head :no_content }
     end
   end
