e7b65d10e325b4d9e9e57e38bba6e4929a9da92a
Kirill qsimpleq Babikhin <kirill.babikhin@gmail.com>
2023-06-19 15:03:33 +0500

    update i18n

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def action_method_to_css_class_color(name = action_name)
+    if %w[new update].include?(name)
+      'success'
+    elsif %w[show edit index].include?(name)
+      'primary'
+    elsif name == 'destroy'
+      'warning'
+    else
+      'default'
+    end
+  end
 end
