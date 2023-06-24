b98c24ff9b17d1a7e5a7318996e1cd2ab49bb113
Kirill qsimpleq Babikhin <kirill.babikhin@gmail.com>
2023-06-24 12:15:16 +0500

    fix comments test and fixture

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -5,7 +5,7 @@ class CommentsControllerTest < ActionDispatch::IntegrationTest
     get '/users/sign_in'
     @user_one = users(:one)
     @post_one = posts(:one)
-    @comment_with_comments = post_comments(:with_comments)
+    @comment_one = post_comments(:one)
     @comment_nested = post_comments(:nested)
     @comment_deep_nested = post_comments(:deep_nested)
 
@@ -15,9 +15,9 @@ class CommentsControllerTest < ActionDispatch::IntegrationTest
   test 'should create comment' do
     post post_comments_url(@post_one), params: {
       post_comment: {
-        post_id: @comment_with_comments.post_id,
-        user_id: @comment_with_comments.user_id,
-        content: @comment_with_comments.content
+        post_id: @comment_one.post_id,
+        user_id: @comment_one.user_id,
+        content: @comment_one.content
       }
     }
 
