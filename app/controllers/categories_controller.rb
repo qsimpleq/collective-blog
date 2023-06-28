358594266b8baf9434abb5872325a35acfc6a8d0
Kirill qsimpleq Babikhin <kirill.babikhin@gmail.com>
2023-06-28 21:27:31 +0500

    categories fixes

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -21,20 +21,39 @@ class CategoriesController < ApplicationController
   # POST /categories or /categories.json
   def create
     @category = Category.new(category_params)
-    @category.save
-    respond_with(@category)
+
+    respond_to do |format|
+      if @category.save
+        format.html { redirect_to category_url(@category), tflash }
+        format.json { render :show, status: :created, location: @category }
+      else
+        format.html { render :new, status: :unprocessable_entity, **tflash(:alert) }
+        format.json { render json: @category.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   # PATCH/PUT /categories/1 or /categories/1.json
   def update
-    @category.update(category_params)
-    respond_with(@category)
+    respond_to do |format|
+      if @category.update(category_params)
+        format.html { redirect_to category_url(@category), tflash }
+        format.json { render :show, status: :ok, location: @category }
+      else
+        format.html { render :edit, status: :unprocessable_entity, **tflash(:alert) }
+        format.json { render json: @category.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   # DELETE /categories/1 or /categories/1.json
   def destroy
     @category.destroy
-    respond_with(@category, location: categories_url)
+
+    respond_to do |format|
+      format.html { redirect_to categories_url, tflash }
+      format.json { head :no_content }
+    end
   end
 
   private
