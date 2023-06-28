40c59958c9d2e95b3cc3e944fba441aaebdce47d
Kirill qsimpleq Babikhin <kirill.babikhin@gmail.com>
2023-06-28 21:28:13 +0500

    post_comment fixes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,30 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!, only: %i[create]
+  before_action :set_post, only: %i[create destroy]
   def show
-    @comment = PostComment.new(params[:id])
-    respond_with(@comment)
   end
 
   def create
-    @comment = PostComment.new(comment_params)
-    @comment.save
-    respond_with(@comment, location: post_path(@comment.post_id, anchor: "post_comment_#{@comment.id}"))
+    @comment = @post.post_comments.build(comment_params)
+
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to post_path(@comment.post_id, anchor: "post_comment_#{@comment.id}"), tflash }
+        format.json { render :show, status: :created, location: @comment }
+      else
+        format.html { render :new, status: :unprocessable_entity, **tflash(:alert) }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   private
 
   def comment_params
     params.require(:post_comment).permit(%i[content post_id user_id ancestry])
+  end
+
+  def set_post
+    @post = Post.find(params[:post_id])
   end
 end
