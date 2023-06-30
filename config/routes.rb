f69c12facd640095799247176c30e3e6b43f9bbc
Kirill qsimpleq Babikhin <kirill.babikhin@gmail.com>
2023-06-30 16:39:10 +0500

    small fixes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_for :users
   resources :categories
-  resources :posts, except: :index do
+  resources :posts do
     resources :comments, only: %i[new create destroy]
     resources :likes, only: %i[create destroy]
   end
