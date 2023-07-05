bf2995ed00d48222fe085fdb75d8db088c518b27
Kirill qsimpleq Babikhin <kirill.babikhin@gmail.com>
2023-07-05 14:42:02 +0500

    fix rubocop issues

--- db/migrate/20230705085330_remove_index_from_categories.rb
+++ db/migrate/20230705085330_remove_index_from_categories.rb
@@ -1,5 +1,11 @@
+# frozen_string_literal: true
+
 class RemoveIndexFromCategories < ActiveRecord::Migration[7.0]
-  def change
+  def up
     remove_index :category, name: 'unique_name'
+  end
+
+  def down
+    add_index :category, :name, name: 'unique_name', unique: true
   end
 end
--- db/migrate/20230705091121_add_index_to_categories.rb
+++ db/migrate/20230705091121_add_index_to_categories.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddIndexToCategories < ActiveRecord::Migration[7.0]
   def change
     add_index :categories, :name
