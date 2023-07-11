0ce16102ea9d99f94b7937ac409b2a79afd750d2
Kirill qsimpleq Babikhin <kirill.babikhin@gmail.com>
2023-07-11 22:42:45 +0500

    Update CategoryTest

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -16,7 +16,23 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @category_one = categories(:one)
+    @name_valid = 'category'
+    @name_too_long = 'a' * 51
+    @name_too_short = ''
+  end
+
+  test 'valid name' do
+    assert_predicate Category.new(name: @name_valid), :valid?
+  end
+
+  test 'invalid name' do
+    assert_not_predicate Category.new(name: @name_too_long), :valid?
+    assert_not_predicate Category.new(name: @name_too_short), :valid?
+  end
+
+  test 'duplicate name' do
+    assert_not_predicate Category.new(name: @category_one.name), :valid?
+  end
 end
