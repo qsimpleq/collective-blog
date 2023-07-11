490da6ee546172c13d9c62d50d5e1e0cc0d674c2
Kirill qsimpleq Babikhin <kirill.babikhin@gmail.com>
2023-07-12 02:00:51 +0500

    Fix ApplicationHelperTest

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -11,7 +11,7 @@ class ApplicationHelperTest < ActionView::TestCase
       update: 'success',
       show: 'primary',
       index: 'primary',
-      destroy: 'danger',
+      destroy: 'warning',
       other: 'default'
     }
 
