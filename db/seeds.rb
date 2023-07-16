bece00a4ff0fc00fd69521cc6564e28e6898a0c8
Kirill qsimpleq Babikhin <kirill.babikhin@gmail.com>
2023-07-16 14:45:33 +0500

    Add console messages for seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,10 +25,14 @@
 
 require 'faker'
 
+Rails.logger.debug '###############',
+                   'Seeding started'
+
 require_relative 'seeds/users'
 require_relative 'seeds/categories'
 require_relative 'seeds/posts'
 require_relative 'seeds/post_comments'
 require_relative 'seeds/post_likes'
 
-Rails.logger.debug 'ok'
+Rails.logger.debug 'Seeding stopped',
+                   '###############'
